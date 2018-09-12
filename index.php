1259ba530b8de9f91086d2467fe6f62b457fad00
Anatolie <anatolie.ghebea@teranet.it>
2018-09-12 17:31:43 +0200

    init

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document single page builder</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    
+</head>
+<body>
+    <h1>Simple single page document builder</h1>
+
+    <div class="document-content-preview">
+        <div class="main-document">
+
+
+        </div>  
+    </div>
+
+    <div class="pg-toolbar">
+        <div class="wrapper">
+            <p> tools: </p>
+
+            <button>Add Chapter</button>
+            <button>Add Section</button>
+            <button>Add Paragraph</button>
+            
+
+        </div>
+    </div>
+
+
+<script src="main.js"></script>
+<script>
+
+    function loadDocPage(name){
+
+    }
+
+    function addChapter(){
+        //add a simple input
+    }
+
+    function addSection(id_chapter = null){
+        //add a simple imput 
+    }
+
+    function addParagraph(id_section = null){
+        //load tiny_mce
+    }
+
+</script>
+
+</body>
+</html>
